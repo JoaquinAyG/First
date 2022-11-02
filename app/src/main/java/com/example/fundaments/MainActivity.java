de75bff3885ce7757eec568ff4c64d246da27e52
Kino <joaquin.ayllongar@gmail.com>
2022-11-02 10:16:35 +0100

    cleanup

--- app/src/main/java/com/example/fundaments/MainActivity.java
+++ app/src/main/java/com/example/fundaments/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.fundaments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
