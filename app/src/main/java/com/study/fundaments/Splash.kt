602fc0db7e570e17969208336c1edd9922e5eba7
kino <joaquin.ayllongar@gmail.com>
2022-11-17 09:36:48 +0100

    spash refactor

--- app/src/main/java/com/study/fundaments/Splash.kt
+++ app/src/main/java/com/study/fundaments/Splash.kt
@@ -3,6 +3,7 @@ package com.study.fundaments
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        openApp()
 
         val bg  = findViewById<ImageView>(R.id.backgroundSpash)
 
@@ -23,12 +25,9 @@ class Splash : AppCompatActivity() {
 
         bg.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate))
 
-        openApp()
-
     }
     private fun openApp() {
-        val handler = Handler()
-        handler.postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@Splash, Login::class.java)
             startActivity(intent)
         }, 5000)
